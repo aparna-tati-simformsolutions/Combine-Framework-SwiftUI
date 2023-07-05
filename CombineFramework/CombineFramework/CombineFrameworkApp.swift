029edb9226bb707c73d0de48c8488559098b66cd
Aparna Tati <aparna.t@simformsolutions.com>
2023-07-05 11:42:58 +0530

    Project and pod setup

---
+++ CombineFramework/CombineFramework/CombineFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineFrameworkApp.swift
+//  CombineFramework
+//
+//  Created by Aparna Tati on 05/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CombineFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
